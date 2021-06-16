a5505edc379ddd71348db27a2a42ab6fc0014329
rahil003 <58912014+rahil003@users.noreply.github.com>
2021-06-16 23:46:18 +0530

    Update SignUp.php

--- SignUp.php
+++ SignUp.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>e-ELECTION</title>
+  <title>ELECTIONS</title>
   <meta http-equiv="content-type" content="text/html; charset=UTF-8">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
